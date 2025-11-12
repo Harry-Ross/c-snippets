d6ea4356f794a9c136f8494dac9dcfbd674b5d25
Harry Ross <hazross@hotmail.com>
2025-11-12 20:03:16 +1100

    ran biome format

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "c-snippets" is now active!');
-	vscode.window.showInformationMessage('Thank you for installing C snippets. Happy coding!');
+	vscode.window.showInformationMessage(
+		"Thank you for installing C snippets. Happy coding!",
+	);
 }
 
 // this method is called when your extension is deactivated
